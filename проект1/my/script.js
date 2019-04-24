e9ded7e3a8d7892fd49a020033210f5b88f00597
yury <susorov.yuriy@gmail.com>
2019-04-24 16:24:54 +0300

    nash 4 kommit

---
+++ проект1/my/script.js
@@ -0,0 +1,7 @@
+//alert("Hello world!");
+//let answer = confirm("Privet");
+let money = prompt("Skolko tebe nujno deneg?", "1000000");
+let time = prompt("Skolko tebe nujno vremeni?", "8-12-2019");
+
+console.log(money);
+console.log(time);
